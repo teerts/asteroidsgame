4534ec1201fcf82e26ecb94409052e0a9534e809
Gerry Grewall <gobind99@gmail.com>
2025-04-21 21:09:27 -0400

    Added up and down movement using unit vector
    - Start with a unit vector pointing straight up from (0,0) to (0,1) -> rotate that vector by the player's rotation so it's pointing in the direction the player is facing -> multiply the player speed by delta time for movement -> add the vector to our position to move the player (triangle).

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
 
@@ -33,4 +33,14 @@ class Player(CircleShape):
             self.rotate(dt*(-1))
 
         if keys[pygame.K_d]:
-            self.rotate(dt) 
+            self.rotate(dt)
+
+        if keys[pygame.K_s]:
+            self.move(dt*(-1)) 
+
+        if keys[pygame.K_w]: 
+            self.move(dt)
+
+    def move(self, dt): 
+        forward = pygame.Vector2(0,1).rotate(self.rotation) 
+        self.position += forward * PLAYER_SPEED * dt
