c41f13adacd66b0e292a8188c88ee602b5dea1be
Gerry Grewall <gobind99@gmail.com>
2025-04-21 20:10:02 -0400

    Cap game at 60 fps using clock.tick
    - This will pause the game loop until 1/60th of a second has passed. Additionally, dt (delta time) is returned for how much time has passed since the last time tick() was called. The purpose of capping FPS is so the game runs on any hardware fast or slow at the same rate. Otherwise on faster machines it would be super quick with error accumulation which would eventually need to be damped. On slower machines, it would feel laggy.

--- main.py
+++ main.py
@@ -10,13 +10,17 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock_obj = pygame.time.Clock() 
+    dt = 0 
+    
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        dt = clock_obj.tick(60)
+        dt /= 1000 # Convert ms to seconds
         
 
 if __name__ == "__main__":
